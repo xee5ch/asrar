2553ab741e51cf6d90b423091d7925214e23ed2a
= <=>
2018-09-09 22:26:09 -0400

    [cli] Use clap to write sample help menu and accept 1 arg.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Asrar")
+        .version("0.1.0")
+        .author("Al S <xee5ch.gh.al@il5.in>")
+        .about("pwgen clone written in Rust")
+        .arg(Arg::with_name("Count")
+                 .required(true)
+                 .takes_value(true)
+                 .index(1)
+                 .help("Number of chars to generate"))
+        .get_matches();
+    let count = matches.value_of("Count").unwrap();
+    println!("{}", count);
 }
